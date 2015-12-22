a9083ef3414b9ce3ec7bb8d563c9bd3d7345fc53
flokosiol <git@flokosiol.de>
2015-12-22 22:11:55 +0100

    menu

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,21 +1,23 @@
 <nav role="navigation">
 
   <ul class="menu cf">
-    <?php foreach($pages->visible() as $p): ?>
-    <li>
-      <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+    <?php foreach($pages->filterBy('menu','1') as $p): ?>
 
-      <?php if($p->hasVisibleChildren()): ?>
-      <ul class="submenu">
-        <?php foreach($p->children()->visible() as $p): ?>
-        <li>
-          <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-      <?php endif ?>
+      <li>
+        <a <?php e($p->isOpen(), ' class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
 
-    </li>
+        <?php if ($p->hasChildren() && $p->children()->filterBy('menu','1')->count()): ?>
+        <ul class="submenu">
+          <?php foreach($p->children()->filterBy('menu','1') as $p): ?>
+          <li>
+            <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
+          </li>
+          <?php endforeach ?>
+        </ul>
+        <?php endif ?>
+
+      </li>
+      
     <?php endforeach ?>
   </ul>
 
